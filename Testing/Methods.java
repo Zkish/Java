a712da08d0d054de2f5c1b03b835e7cc763367e1
Zkish <zachary.kish@gmail.com>
2024-01-14 05:23:07 -0500

    adding methods section for practice

---
+++ Testing/Methods.java
@@ -0,0 +1,13 @@
+package Testing;
+
+// my methods same thing as functions in python
+public class Methods {
+    static void MyMethod(){
+        System.out.println("Hello");
+    }
+
+    // here is the main code
+    public static void main(String[] args){
+        MyMethod();
+    }
+}
